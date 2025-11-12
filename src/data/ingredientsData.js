0d44a88543d96f2bff1143fa65fafd589456b354
banny-suh <banny.suh@gmail.com>
2025-11-12 15:20:49 +0900

    feat: 기존 dummy data 수정

--- src/data/ingredientsData.js
+++ src/data/ingredientsData.js
@@ -1,6 +1,12 @@
 export const ingredientsData = {
     "ingredients": [
-        { "id": "1", "name": "강력분-우리밀(오가그레인) 10kg", "gramsPurchased": 10000, "price": 40000 },
-        { "id": "2", "name": "소금", "gramsPurchased": 1000, "price": 3000 },
+        { "id": "1", "name": "강력분-우리밀(오가그레인)", "gramsPurchased": 10000, "price": 40000 },
+        { "id": "2", "name": "곰표 강력 다목적용", "gramsPurchased": 2500, "price": 8950 },
+        { "id": "8", "name": "밥스브레드밀 통밀", "gramsPurchased": 2270, "price": 14900 },
+        { "id": "5", "name": "앵커 버터 (냉동)", "gramsPurchased": 454, "price": 8000 },
+        { "id": "3", "name": "소금", "gramsPurchased": 1000, "price": 3000 },
+        { "id": "4", "name": "우유", "gramsPurchased": 1000, "price": 2920 },
+        { "id": "6", "name": "설탕-백설탕", "gramsPurchased": 1000, "price": 2800 },
+        { "id": "7", "name": "계란", "gramsPurchased": 20, "price": 7000 },
     ]
 }
---
+++ src/data/recipesData.js
@@ -0,0 +1,19 @@
+export const recipesData = {
+    "recipes": [
+        {
+            "id": "1",
+            "name": "사워도우",
+            "count": 1,
+            "items": [
+                {
+                    "ingredientId": "1",
+                    "gramsUsed": 500
+                },
+                {
+                    "ingredientId": "2",
+                    "gramsUsed": 10
+                }
+            ]
+        }
+    ]
+}
